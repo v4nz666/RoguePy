94bb8f1c6e1d54105745c928ee807c9ffa6f5fc7
jripley <jripley@azorus.com>
2016-01-27 20:37:43 -0400

    Add base class :/ - #3

--- Demo/GameStates/Quit.py
+++ Demo/GameStates/Quit.py
@@ -7,8 +7,8 @@ import RoguePy.State.GameState as GameState
 class Quit(GameState):
   def __init__(self, name, manager, ui):
     super(Quit, self).__init__(name, manager, ui)
-    self.addHandler('quit', 1, self.tick)
+    self.addHandler('quit', 1, self.quit)
 
-  def tick(self):
+  def quit(self):
     print "Quiting!"
     sys.exit()
